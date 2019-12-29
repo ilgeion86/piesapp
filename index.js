77b805f4e7e346828719e215091b915c3adcadee
Vy <ks031088@gmail.com>
2019-12-30 01:32:34 +0500

    Added self-signed ssl certificate and created https connection

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require('dotenv').config()
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
 const app = express();
 const homeRouter = require('./routes/home-router')
+const certificates = {
+    cert: fs.readFileSync('./certificates/cert.crt'),
+    key: fs.readFileSync('./certificates/private.key')
+}
 
 app.use('/', homeRouter)
 
-app.listen(process.env.PORT, process.env.HOST, () => {
+const $app_https = https.createServer(certificates, app).listen(process.env.PORT, process.env.HOST)
+
+$app_https.on('listening', () => {
     console.log("Server is working", 'PORT=', process.env.PORT, " HOST=", process.env.HOST)
 })
