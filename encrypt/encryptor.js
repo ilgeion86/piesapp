4ed61a7a530d8ff4f4842215642b0e3f46b29302
Vy <ks031088@gmail.com>
2019-12-30 14:56:53 +0500

    Updated encryptor.js

--- encrypt/encryptor.js
+++ encrypt/encryptor.js
@@ -1,16 +1,22 @@
 const crypto = require('crypto')
 
 module.exports.createHash = function(password) {
-    this.salt = Math.random(new Date().valueOf() * Math.random);
+    this.salt = Math.round(
+        (new Date().valueOf() * Math.random())
+    ) + ''
+
     this.hash = crypto.createHash('sha512')
-        .update(password + salt)
+        .update(password + this.salt)
         .digest('hex')
-    return {
-        salt: this.salt,
-        hash: this.hash
-    }
+
+    return { salt: this.salt, hash: this.hash }
 }
 
-module.exports.compareHash = function(salt, interror_hash, external_hash) {
+module.exports.compareHash = function(salt, interior_hash, external_password) {
+    this.hash = crypto.createHash('sha512')
+        .update(external_password + salt)
+        .digest('hex')
+
+    if (this.hash === interior_hash) return true
 
 }
