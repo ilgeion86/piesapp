b23ea3c5074ee01dfb34dc7aacf335c6f4e5582a
Vy <ks031088@gmail.com>
2019-12-31 17:19:41 +0500

    Created model/users.js

---
+++ model/users.js
@@ -0,0 +1,43 @@
+const db = require('../configs/db-connect').initdb()
+
+/**
+ * Users it is a table of database
+ * @constructor
+ * @param {smallserial} uid - unique identifier
+ * @param {string} name - unique name of user
+ * @param {string} hash - password in hashed view
+ * @param {string} salt - modificator of enter to hash function
+ * @param {roles} role - modificator of enter to hash function
+ * @param {roles} - ENUM('super', 'admin', 'user', 'guest')
+ */
+function Users() {
+    this.uid,
+        this.name,
+        this.hash,
+        this.salt,
+        this.role
+}
+
+/**
+ * Function getUserByName search user by indicated name
+ * field name is unique
+ */
+Users.prototype.getUserByName = async function(name, callback) {
+    const client = await db.connect()
+    const query = "SELECT * FROM USERS WHERE name = $1"
+    try {
+        const data = await db.query(query, [name])
+        callback(data.rows[0])
+    } catch (e) {
+        console.log(e)
+    } finally {
+        client.release()
+    }
+}
+
+module.exports = Users
+
+//let quert = new Users()
+//quert.getUserByName('Vy', (data) => {
+//    console.log(data)
+//})
