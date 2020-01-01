7d3e572fe183575e0ad44d2310cc86ac1ffedf0d
Vy <ks031088@gmail.com>
2020-01-01 12:26:14 +0500

    Minor file changes

--- controllers/home-controllers.js
+++ controllers/home-controllers.js
@@ -1,4 +1,6 @@
-const Users = new require('../model/users')
+const getRes = require('../responses/getResponses')
+const postRes = require('../responses/postResponses')
+const delRes = require('../responses/deleteResponses')
 this.get = Object.create(null)
 this.delete = Object.create(null)
 this.post = Object.create(null)
@@ -9,19 +11,17 @@ this.get.index = function(req, res) {
     })
 }
 this.get.login = function(req, res) {
-    res.render('login.ejs', { error: req.flash('error') || [] })
-
+    getRes.login(req, res)
 }
 module.exports.get = this.get
 
 this.post.login = function(req, res) {
-    res.redirect('/')
+    postRes.login(req, res)
 }
 
 module.exports.post = this.post
 
 this.delete.logout = function(req, res) {
-    req.logOut()
-    res.redirect('/login')
+    delRes.logout(req, res)
 }
 module.exports.delete = this.delete
