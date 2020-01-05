48000d5952ae3610e421ce0fc566e3f18fc4d904
Vy <ks031088@gmail.com>
2020-01-05 16:05:04 +0500

    Created directory for home respoponses

--- responses/homeResponses/getResponses.js
+++ responses/homeResponses/getResponses.js
@@ -1,4 +1,5 @@
-const Users = new require('../model/users')
+const Users = require("../../model/users")
+
 module.exports.login = function(req, res) {
     res.render('login', { error: req.flash('error') || [] })
 }
