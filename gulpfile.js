5bf317253ac2fc492dc62e573705295bf9e12eec
Vy <ks031088@gmail.com>
2020-01-11 09:16:24 +0500

    Added gulp tastks for watching to *.styl

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,26 @@ gulp.task('styl', function() {
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('./public/css'))
 })
-
+gulp.task('styl_mobile', function() {
+    return gulp.src('stylus/mobile/*.styl')
+        .pipe(sourcemaps.init())
+        .pipe(stylus())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./public/css/mobile'))
+})
+gulp.task('styl_desctop', function() {
+    return gulp.src('stylus/desctop/*.styl')
+        .pipe(sourcemaps.init())
+        .pipe(stylus())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./public/css/desctop'))
+})
 gulp.task('watch', function() {
-    gulp.watch('stylus/*.styl', gulp.series('styl'))
+    gulp.watch(
+        [
+            'stylus/*.styl',
+            'stylus/mobile/*.styl',
+            'stylus/desctop/*.styl'
+        ],
+        gulp.series('styl', 'styl_desctop'))
 })
