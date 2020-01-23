1969ff838025db7eca0f7982eff0eb4f1c8888fc
Vy <ks031088@gmail.com>
2020-01-24 02:06:25 +0500

    Created script for open tabs and styles for order info panel

---
+++ public/js/openTab.js
@@ -0,0 +1,8 @@
+function openTab(cityName) {
+    var i;
+    var x = document.getElementsByClassName("tabs");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(cityName).style.display = "block";
+}
