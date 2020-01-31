f7a30e0c296c3c26b5f7e9a7595297f442375bec
Vy <ks031088@gmail.com>
2020-01-31 14:13:09 +0500

    Created filling of client details tab

---
+++ formsModels/orderInfoForm/tabs/details.js
@@ -0,0 +1,15 @@
+let obj = Object.create(null)
+
+obj = {
+    1: ["Карта", "clientCard"],
+    2: ["Телефон", "clientPhone"],
+    3: ["Имя", "clientName"],
+    4: ["Улица", "clientStreetName"],
+    5: ["Дом", "clientDuldNnumber"],
+    6: ["Подъезд", "clientFrontDoor"],
+    7: ["Этаж", "clientFloor"],
+    8: ["Кв. (офис)", "clientRoomNumber"],
+    9: ["Примечание", "clientAdditionalNotes"]
+}
+
+module.exports = obj
--- responses/homeResponses/getResponses.js
+++ responses/homeResponses/getResponses.js
@@ -1,5 +1,6 @@
 const Users = require("../../model/users")
 const Categories = require("../../model/categories")
+const tabDetails = require("../../formsModels/orderInfoForm/tabs/details")
 
 module.exports.login = function(req, res) {
     res.render('login', {
@@ -12,6 +13,7 @@ module.exports.index = function(req, res) {
     categories.getCategories((allCategories) => {
         res.render('index', {
             categories: allCategories,
+            tabDetails: tabDetails,
             user: req.user.name,
             role: req.user.role
         })
